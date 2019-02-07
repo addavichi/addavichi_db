f13cb5d01eb80f7cc4a7a85120ad0cb2f3d8a5b4
davichiar <ardabitchy02@naver.com>
2019-02-07 12:49:11 +0900

    fianl test

---
+++ Search.php
@@ -0,0 +1,16 @@
+<?php
+    $con = mysqli_connect("localhost", "davichiar1", "a1b1c1**", "davichiar1");
+
+    //안드로이드 앱으로부터 아래 값들을 받음
+    $name = $_POST['name'];
+
+    //insert 쿼리문을 실행함
+    $statement = mysqli_prepare($con, "INSERT INTO search VALUES (?)");
+    mysqli_stmt_bind_param($statement, "s", $name);
+    mysqli_stmt_execute($statement);
+
+    $response = array();
+    $response["success"] = true;
+
+    echo json_encode($response);
+?>
